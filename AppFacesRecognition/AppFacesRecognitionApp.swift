d3f4dcbd654417952189c46541cb61e46b3ba6ba
Daniela Ramos <daniramos5566@hotmail.com>
2023-09-29 12:40:49 -0600

    Initial Commit

---
+++ AppFacesRecognition/AppFacesRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppFacesRecognitionApp.swift
+//  AppFacesRecognition
+//
+//  Created by Daniela Ramos Garcia on 29/09/23.
+//
+
+import SwiftUI
+
+@main
+struct AppFacesRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
